72d89ba0fda0c7f23eebf69cca9f4b9f560611c1
Storius <hieupt1003@gmail.com>
2025-06-05 22:22:51 +0700

    Change note content type from Delta object to Html string

---
+++ Backend/src/models/category.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Category name is required"],
+			trim: true,
+		},
+
+		color: {
+			type: String,
+			required: true,
+			enum: [
+				"#e67e22",
+				"#b4b800",
+				"#6ea204",
+				"#1abc9c",
+				"#3498db",
+				"#3b82f6",
+				"#a78bfa",
+				"#e879f9",
+				"#e74c3c",
+			],
+			default: "#e67e22",
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
